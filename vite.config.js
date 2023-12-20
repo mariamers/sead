2bc557144e456f8f7c4bcc83d09a375b0f631be0
maria <dudahmers@gmail.com>
2023-12-20 13:19:34 -0300

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/sead"
-})
+  base:"/sead",
+  resolve: {
+    //alias: {
+    //  "~": path.resolve(__dirname, "node_modules"),
+ //     "@": path.resolve(__dirname, "src"),
+   // },
+  },
+  build: {
+    chunkSizeWarningLimit: 1900,
+  },
+});
+
